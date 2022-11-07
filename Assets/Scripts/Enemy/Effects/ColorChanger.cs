75db09b2131e8fb8571c325d48f95426b5006317
dexeltod <69011194+dexeltod@users.noreply.github.com>
2022-11-07 14:23:54 +0200

    Moved the script

--- Assets/Scripts/Enemy/Effects/ColorChanger.cs
+++ Assets/Scripts/Enemy/Effects/ColorChanger.cs
@@ -3,47 +3,41 @@ using UnityEngine;
 
 public class ColorChanger : MonoBehaviour
 {
-    [SerializeField] private Material _hitMaterial;
-    [SerializeField] private float _colorChangingTime;
-    
-    private SpriteRenderer _spriteRenderer;
-    private Material _originalMaterial;
-    private Color _hitColor;
-    private Coroutine _currentColorCoroutine;
-    private Enemy _enemy;
+	[SerializeField] private Material _hitMaterial;
+	[SerializeField] private float _colorChangingTime;
 
-    private void Awake()
-    {
-        _spriteRenderer = GetComponent<SpriteRenderer>();
-        _originalMaterial = _spriteRenderer.material;
-        _enemy = GetComponent<Enemy>();
-    }
+	private SpriteRenderer _spriteRenderer;
+	private Material _originalMaterial;
+	private Color _hitColor;
+	private Coroutine _currentColorCoroutine;
+	private Enemy _enemy;
 
-    private void OnEnable()
-    {
-        _enemy.WasHit += OnColorChange;
-    }
+	private void Awake()
+	{
+		_spriteRenderer = GetComponent<SpriteRenderer>();
+		_originalMaterial = _spriteRenderer.material;
+		_enemy = GetComponent<Enemy>();
+	}
 
-    private void OnDisable()
-    {
-        _enemy.WasHit -= OnColorChange;
-    }
+	private void OnEnable() =>
+		_enemy.WasHit += OnColorChange;
 
-    private void OnColorChange()
-    {
-        if (_currentColorCoroutine != null)
-        {
-            StopCoroutine(_currentColorCoroutine);
-        }
-        
-        _currentColorCoroutine = StartCoroutine(ChangeColorRoutine());
-    }
-    
-    private IEnumerator ChangeColorRoutine()
-    {
-        _spriteRenderer.material = _hitMaterial;
-        yield return new WaitForSeconds(_colorChangingTime);
-        _spriteRenderer.material = _originalMaterial;
-        _currentColorCoroutine = null;
-    }
+	private void OnDisable() =>
+		_enemy.WasHit -= OnColorChange;
+
+	private void OnColorChange()
+	{
+		if (_currentColorCoroutine != null) 
+			StopCoroutine(_currentColorCoroutine);
+
+		_currentColorCoroutine = StartCoroutine(ChangeColorRoutine());
+	}
+
+	private IEnumerator ChangeColorRoutine()
+	{
+		_spriteRenderer.material = _hitMaterial;
+		yield return new WaitForSeconds(_colorChangingTime);
+		_spriteRenderer.material = _originalMaterial;
+		_currentColorCoroutine = null;
+	}
 }
